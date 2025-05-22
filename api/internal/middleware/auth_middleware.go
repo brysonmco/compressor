7c242abac55156955df40aba46d41ea5df625af4
Bryson <55819926+brysonmco@users.noreply.github.com>
2025-05-22 14:08:45 -0600

    added email verification to Auth Middleware

--- api/internal/middleware/auth_middleware.go
+++ api/internal/middleware/auth_middleware.go
@@ -24,6 +24,7 @@ func NewAuthMiddleware(auth *auth.Auth, database *db.Database) *AuthMiddleware {
 
 func (m *AuthMiddleware) Protected(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Validate token
 		id, err := m.Auth.ValidateAccessToken(strings.TrimPrefix(r.Header.Get("Authorization"), "Bearer "))
 		if err != nil && errors.Is(err, errors.New("expired_token")) {
 			utils.WriteError(w, r, http.StatusUnauthorized, "token has expired", "expired_token", nil)
@@ -33,7 +34,34 @@ func (m *AuthMiddleware) Protected(next http.Handler) http.Handler {
 			return
 		}
 
-		ctx := context.WithValue(r.Context(), "userID", id)
+		// Ensure their email is valid
+		user, err := m.Database.FindUserByID(r.Context(), id)
+		if err != nil {
+			utils.WriteError(w, r, http.StatusInternalServerError, "error fetching user", "user_not_found", nil)
+		}
+		if !user.EmailVerified {
+			utils.WriteError(w, r, http.StatusUnauthorized, "email not verified", "email_not_verified", nil)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "userId", id)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func (m *AuthMiddleware) ProtectedWithoutEmailVerification(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Validate token
+		id, err := m.Auth.ValidateAccessToken(strings.TrimPrefix(r.Header.Get("Authorization"), "Bearer "))
+		if err != nil && errors.Is(err, errors.New("expired_token")) {
+			utils.WriteError(w, r, http.StatusUnauthorized, "token has expired", "expired_token", nil)
+			return
+		} else if err != nil {
+			utils.WriteError(w, r, http.StatusUnauthorized, "invalid token", "invalid_token", nil)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "userId", id)
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
